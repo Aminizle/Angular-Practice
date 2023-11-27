02e0802c4c264cdd3ee226c700fc4555cfb174a4
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-11-26 21:07:04 -0500

    Update main.ts

--- homes-app/src/main.ts
+++ homes-app/src/main.ts
@@ -1,11 +1,21 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+1import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import routeConfig from './app/routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
+
+
     provideRouter(routeConfig)
   ]
 })
-  .catch(err => console.error(err));
+  .catch(err => console.erro14r(err));
+0L7K;.8IM9 J,J8OIM0PL,;.-['/']20.1.
+215846
+.3./216509
+
+|
+
+\]+",. F ,
+\
